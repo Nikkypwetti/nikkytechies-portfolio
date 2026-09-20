b74a9eb08031a2da923ea68e7f54f32a5c4025ec
Ganiyu Basirat Olanike <129284834+Nikkypwetti@users.noreply.github.com>
2026-09-20 10:54:07 +0100

    feat: surface Manus Agent Skill portfolio project

--- data/all-projects.ts
+++ data/all-projects.ts
@@ -1,4 +1,5 @@
 import { hubspotProject } from "./hubspot-project";
+import { manusRevopsProject } from "./manus-revops-project";
 import { projects } from "./projects";
 
-export const allProjects = [hubspotProject, ...projects];
+export const allProjects = [manusRevopsProject, hubspotProject, ...projects];
